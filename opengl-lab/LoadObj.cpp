b539bd6fad376a126858834d5feb4eab18236115
bartonu4 <bartonu4@gmail.com>
2015-11-25 19:45:19 +0200

    edited load obj

--- opengl-lab/LoadObj.cpp
+++ opengl-lab/LoadObj.cpp
@@ -11,24 +11,50 @@ LoadObj::~LoadObj()
 }
 bool LoadObj::loadObj()
 {
-	vector<vec3<float>> temp_vert;
-	vector<vec2<float>> tem_uvs;
-	vector<vec3<float>> temp_norm;
+	vector<vec3f> temp_vert;
+	vector<vec2f> temp_uvs;
+	vector<vec3f> temp_norm;
 	ifstream obj(filename, ifstream::in);
 	while (!obj.eof())
 	{
 		char line[128];
 		stringstream sstr;
 		obj.getline(line,128);
-		vec3<float> vertice;
-		vec2<float> uv;
-		vec3<float>normale;
+		vec3f vertice;
+		vec2f uv;
+		vec3f normale;
 		
-		if (1==strcmp(line, "v"))
+		vec3i vertices_ind, uvs_ind, normales_ind;
+		if (line[0]=='v'&&line[1]==' ')
 		{
 			
 			int result = sscanf(line, "v %f %f %f\n",&vertice.x,&vertice.y,&vertice.z);
 			temp_vert.push_back(vertice);
+		} 
+		if (line[0]=='v'&&line[1] == 't' )
+		{
+
+			int result = sscanf(line, "vt %f %f\n", &uv.x, &uv.y);
+			temp_uvs.push_back(uv);
+		}
+		if (line[0]=='v' && line[1] == 'n' )
+		{
+
+			int result = sscanf(line, "vn %f %f %f\n", &normale.x, &normale.y, &normale.z);
+			temp_norm.push_back(normale);
+		}
+		if (line[0]==102)
+		{
+
+			int result = sscanf(line, "f %d/%d/%d %d/%d/%d %d/%d/%d/n", 
+				&vertices_ind.x, &uvs_ind.x, &normales_ind.x, 
+				&vertices_ind.y, &uvs_ind.y, &normales_ind.y,
+				&vertices_ind.z, &uvs_ind.z, &normales_ind.z);
+			
+			
+			vertices.push_back(vec3f(temp_vert[vertices_ind.x - 1].x, temp_vert[vertices_ind.y - 1].y, temp_vert[vertices_ind.z - 1].z));		
+			normales.push_back(vec3f(temp_norm[normales_ind.x - 1].x, temp_vert[normales_ind.y - 1].y, temp_vert[normales_ind.z - 1].z));
+			uvs.push_back(vec2f(temp_uvs[uvs_ind.x - 1].x, temp_uvs[uvs_ind.y - 1].y));
 		}
 		
 	}
--- opengl-lab/LoadObj.h
+++ opengl-lab/LoadObj.h
@@ -10,21 +10,28 @@ using std::string;
 using std::stringstream;
 
 template<typename T>
-struct vec2{
+struct my_vec2{
+	my_vec2(){};
+	my_vec2(T _x, T _y) :x(_x), y(_y){};
 	T x{ 0 }, y{ 0 };
 };
 template<typename T>
-struct vec3{
+struct my_vec3{
+	my_vec3(){};
+	my_vec3(T _x, T _y, T _z) :x(_x), y(_y), z(_z){};
+	
 	T x{ 0 }, y{ 0 }, z{ 0 };
 };
-
+typedef my_vec2<float> vec2f;
+typedef my_vec3<float> vec3f;
+typedef my_vec3<int> vec3i;
 class LoadObj
 {
 private:
 	string filename;
-	vector<vec3<float>> vertices;
-	vector < vec2<float>> uvs;
-	vector < vec3<float>> normales;
+	vector<vec3f> vertices;
+	vector < vec2f> uvs;
+	vector < vec3f> normales;
 public:
 	LoadObj();
 	LoadObj(string name) :filename(name){};
